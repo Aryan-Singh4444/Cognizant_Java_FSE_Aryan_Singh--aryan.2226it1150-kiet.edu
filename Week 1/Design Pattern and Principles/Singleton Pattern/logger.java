398a3c90e3478303b249f7bd575654af6d022a30
Aryan Singh <aryan.2226it1150@kiet.edu>
2025-06-22 15:45:04 +0530

    Add files via upload

---
+++ Week 1/Design Pattern and Principles/Singleton Pattern/logger.java
@@ -0,0 +1,20 @@
+// File: Logger.java
+public class Logger {
+    
+    private static Logger instance = new Logger();
+
+    
+    private Logger() {
+        System.out.println("Logger Initialized");
+    }
+
+    
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    
+    public void log(String message) {
+        System.out.println("[LOG]: " + message);
+    }
+}
